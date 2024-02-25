389295c98fa91a60977465ebafb1782f4fc65f0a
ian <ianfranco9live.com>
2024-02-25 05:45:21 -0300

    Update 2.5: Traducciones

--- assets/js/module.js
+++ assets/js/module.js
@@ -1,6 +1,6 @@
 /**
  * @license Apache Version 2.0
- * @fileoverview Menage all routes
+ * @fileoverview Manage all routes
  * @copyright Notorious 2024 All rights reserved
  * @author Notorious <ianfranco9@live.com>
  */
@@ -8,34 +8,34 @@
 'use strict';
 
 export const weekDayNames = [
-  "Sunday",
-  "Monday",
-  "Tuesday",
-  "Wednesday",
-  "Thursday",
-  "Friday",
-  "Saturday"
+  "Domingo",
+  "Lunes",
+  "Martes",
+  "Miércoles",
+  "Jueves",
+  "Viernes",
+  "Sábado"
 ];
 
 export const monthNames = [
-  "Jan",
+  "Ene",
   "Feb",
   "Mar",
-  "Apr",
+  "Abr",
   "May",
   "Jun",
   "Jul",
-  "Aug",
+  "Ago",
   "Sep",
   "Oct",
   "Nov",
-  "Dec"
+  "Dic"
 ];
 
 /**
  * @param {number} dateUnix Unix date in seconds
  * @param {number} timezone Timezone shift from UTC in seconds
- * @returns {string} Date String. formate: "Sunday 10, Jan"
+ * @returns {string} Date String. format: "Domingo 10, Ene"
  */
 export const getDate = function (dateUnix, timezone) {
   const date = new Date((dateUnix + timezone) * 1000);
@@ -48,7 +48,7 @@ export const getDate = function (dateUnix, timezone) {
 /**
  * @param {number} timeUnix Unix date in seconds
  * @param {number} timezone Timezone shift from UTC in seconds
- * @returns {string} Time string. formate: "HH:MM AM/PM"
+ * @returns {string} Time string. format: "HH:MM AM/PM"
  */
 export const getTime = function (timeUnix, timezone) {
   const date = new Date((timeUnix + timezone) * 1000);
@@ -62,7 +62,7 @@ export const getTime = function (timeUnix, timezone) {
 /**
  * @param {number} timeUnix Unix date in seconds
  * @param {number} timezone Timezone shift from UTC in seconds
- * @returns {string} Time string. formate: "HH AM/PM"
+ * @returns {string} Time string. format: "HH AM/PM"
  */
 export const getHours = function (timeUnix, timezone) {
   const date = new Date((timeUnix + timezone) * 1000);
@@ -73,33 +73,33 @@ export const getHours = function (timeUnix, timezone) {
 }
 
 /**
- * @param {number} mps Metter per seconds
- * @returns {number} Kilometer per hours
+ * @param {number} mps Meter per second
+ * @returns {number} Kilometer per hour
  */
 export const mps_to_kmh = mps => {
-  const mph = mps * 3600;
-  return mph / 1000;
+  const kph = mps * 3600 / 1000;
+  return kph;
 }
 
 export const aqiText = {
   1: {
-    level: "Good",
-    message: "Air quality is considered satisfactory, and air pollution poses little or no risk"
+    level: "Bueno",
+    message: "La calidad del aire se considera satisfactoria y la contaminación del aire no representa un riesgo o es mínimo."
   },
   2: {
-    level: "Fair",
-    message: "Air quality is acceptable; however, for some pollutants there may be a moderate health concern for a very small number of people who are unusually sensitive to air pollution."
+    level: "Regular",
+    message: "La calidad del aire es aceptable; sin embargo, puede haber cierta preocupación de salud para un pequeño número de personas sensibles a la contaminación del aire."
   },
   3: {
-    level: "Moderate",
-    message: "Members of sensitive groups may experience health effects. The general public is not likely to be affected."
+    level: "Moderado",
+    message: "Los miembros de grupos sensibles pueden experimentar efectos en la salud. El público en general es menos probable que se vea afectado."
   },
   4: {
-    level: "Poor",
-    message: "Everyone may begin to experience health effects; members of sensitive groups may experience more serious health effects"
+    level: "Malo",
+    message: "Cualquier persona puede comenzar a experimentar efectos en la salud; los miembros de grupos sensibles pueden experimentar efectos más graves."
   },
   5: {
-    level: "Very Poor",
-    message: "Health warnings of emergency conditions. The entire population is more likely to be affected."
+    level: "Muy Malo",
+    message: "Advertencias de salud por condiciones de emergencia. Es probable que toda la población se vea afectada."
   }
 }
